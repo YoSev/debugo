9fca676d46c71c53b11e7c813626cbe0b05de844
Yo Sev <severinsiebertz@googlemail.com>
2025-05-24 23:09:27 +0200

    feat: next version

---
+++ color.go
@@ -0,0 +1,47 @@
+package debugo
+
+import (
+	"sync"
+	"time"
+
+	"github.com/fatih/color"
+	"golang.org/x/exp/rand"
+)
+
+var fgColors = []color.Attribute{
+	color.FgRed,
+	color.FgGreen,
+	color.FgYellow,
+	color.FgBlue,
+	color.FgMagenta,
+	color.FgCyan,
+
+	color.FgHiBlack,
+	color.FgHiRed,
+	color.FgHiGreen,
+	color.FgHiYellow,
+	color.FgHiBlue,
+	color.FgHiMagenta,
+	color.FgHiCyan,
+	color.FgHiWhite,
+}
+
+var (
+	fgColorMap = make(map[string]*color.Color)
+	fgColorMux sync.RWMutex
+)
+
+func getRandomColor(namespace string) *color.Color {
+	fgColorMux.Lock()
+	defer fgColorMux.Unlock()
+
+	if c, ok := fgColorMap[namespace]; ok {
+		return c
+	}
+
+	src := rand.NewSource(uint64(time.Now().UnixNano()))
+	r := rand.New(src)
+	c := color.New(fgColors[r.Intn(len(fgColors))])
+	fgColorMap[namespace] = c
+	return c
+}
